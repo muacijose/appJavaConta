92454118a7bd184dde2bd7512c249c58db7bee81
linux <muaci@outlook.com>
2023-07-31 12:33:28 -0300

    inicializar dados do client

--- untitled/src/ContaBacaria.java
+++
@@ -1,5 +0,0 @@
-public class ContaBacaria {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
---
+++ untitled/src/ContaBancaria.java
@@ -0,0 +1,13 @@
+public class ContaBancaria {
+    public static void main(String[] args) {
+        String nome = "Muaci josé ";
+        String tipoConta = "Corrente";
+        double saldoConta = 1599.99;
+        System.out.println("************************************\n");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("saldo da conta: " + saldoConta);
+        System.out.println("\n************************************");
+
+    }
+}
